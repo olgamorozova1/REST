5c27d0dc209feaea2fa1e092ed484024a03baa7d
olgamorozova1 <olgamorozova1>
2021-04-01 22:31:36 +0300

    Fixes

--- src/main/java/by/issoft/training/Main.java
+++ src/main/java/by/issoft/training/Main.java
@@ -3,12 +3,12 @@ package by.issoft.training;
 import by.issoft.training.authorization.Scope;
 import by.issoft.training.authorization.TokenGenerator;
 
-import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         TokenGenerator tokenGenerator = TokenGenerator.getInstance();
-        HashMap<Scope, String> tokens = tokenGenerator.createAccessToken(Scope.READ);
+        Map<Scope, String> tokens = tokenGenerator.createAccessToken(Scope.READ);
         tokens.putAll(tokenGenerator.createAccessToken(Scope.WRITE));
     }
 }
